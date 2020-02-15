d33a40ad03ae0465b81ca2995109ef8445951329
dengwenhao <dengwenhao1105@gmail.com>
2020-02-15 12:32:59 +0800

    忘记密码 界面初始化

---
+++ app/src/main/java/com/mystery/model/ForgetModel.java
@@ -0,0 +1,10 @@
+package com.mystery.model;
+
+import androidx.databinding.BaseObservable;
+
+/**
+ * Created by Vindicated-Rt
+ * 2020-02-15 12:08
+ */
+public class ForgetModel extends BaseObservable {
+}
