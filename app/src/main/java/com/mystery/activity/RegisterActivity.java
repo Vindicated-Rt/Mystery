b8373c6a556e0e20c852d11d38d4a50572a3f8a9
dengwenhao <dengwenhao1105@gmail.com>
2020-02-16 15:33:02 +0800

    布局 整理

--- app/src/main/java/com/mystery/activity/RegisterActivity.java
+++ app/src/main/java/com/mystery/activity/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.mystery.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.mystery.base.BaseUrl;
@@ -15,5 +16,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void closeRegister(View view) {
+        onBackPressed();
     }
 }
