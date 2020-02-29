41fa329b8643afd23b084c0b2ce00bb87a079616
dengwenhao <dengwenhao1105@gmail.com>
2020-02-29 21:13:24 +0800

    blog界面 完成并简单测试数据

--- app/src/main/java/com/mystery/fragment/BlogFragment.java
+++ app/src/main/java/com/mystery/fragment/BlogFragment.java
@@ -1,17 +1,27 @@
 package com.mystery.fragment;
 
 
+import android.graphics.Rect;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mystery.R;
+import com.mystery.adapter.BlogAdapter;
 import com.mystery.databinding.FragmentBlogBinding;
+import com.mystery.model.BlogModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlogFragment extends Fragment {
 
@@ -26,13 +36,27 @@ public class BlogFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         blogBinding = DataBindingUtil
-                .inflate(inflater,R.layout.fragment_blog,container,false);
+                .inflate(inflater, R.layout.fragment_blog, container, false);
         initView();
         return blogBinding.getRoot();
     }
 
-    private void initView(){
-
+    private void initView() {
+        blogBinding.blogRv.setLayoutManager(new LinearLayoutManager(getContext()));
+        List<BlogModel> blogModels = new ArrayList<>();
+        BlogAdapter blogAdapter = new BlogAdapter(getContext(), blogModels);
+        blogBinding.blogRv.setAdapter(blogAdapter);
+        blogBinding.blogRv.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                if (parent.getChildAdapterPosition(view) == 0) {
+                    outRect.top = 20;    //最上方item不设置间距
+                }
+                outRect.left = 20;
+                outRect.bottom = 20;
+                outRect.right = 20;
+            }
+        });
     }
 
 }
